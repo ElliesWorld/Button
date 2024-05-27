862aa623eea22cd06749f5cde4690352d47c55ab
Ellie <146091262+ElliesWorld@users.noreply.github.com>
2024-05-27 13:42:50 +0200

    Update button.h

--- lib/button/button.h
+++ lib/button/button.h
@@ -11,26 +11,10 @@
 
 #define SAMPLES 5
 
-/**
- * @brief This function is used to initialize the module.
- *
- * @param pin The pin number used to connect a button.
- */
 void button_init(uint8_t pin);
 
-/**
- * @brief This function is used to update the debounced state of the button.
- *        This function is called intervally by the client.
- *
- */
 void button_update_state(void);
 
-/**
- * @brief This function is used to get the debounced state of the button.
- *
- * @return uint8_t The state; BUTTON_UNINITITIALIZED if the module has not been initialized, otherwise
- *                            BUTTON_PRESSED, BUTTON_RELEASED, BUTTON_RISING_EDGE or BUTTON_FALLING_EDGE
- */
 uint8_t button_get_state(void);
 
 #endif /* BUTTON_H */
